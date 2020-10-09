f25574d22e551bcb367e8ee6429d8793e216cab3
Richard <richghorn@gmail.com>
2020-10-09 11:29:42 +0200

    Upload

---
+++ Python A-Z Challanges/Numbers/Prime Factorization/Prime Factorization.py
@@ -0,0 +1,13 @@
+lst_prime = [2, 3, 5, 7, 11]
+lst_factors = []
+
+#user inputs number
+print("Please insert a number to undergo Prime Factorization")
+user_input = int(input())
+
+#prime factorisation of user_input vs lst_prime
+
+#if a factor, add factor to the lst_factors
+#repeat until answer is 1
+
+#print out lst_factors
